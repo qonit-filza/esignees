ba26df2b371eb8c75a42e3a8a39da8dd92960cdf
ghazytp <ghazytp@gmail.com>
2023-02-06 18:01:48 +0700

    create organization preview

--- src/views/OrganizationPage.jsx
+++ src/views/OrganizationPage.jsx
@@ -1,3 +1,14 @@
 export default function OrganizationPage() {
-  return <>asfads</>;
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <p>Organization Name</p>
+        <p className="text-2xl font-semibold">PT SUMBER JAYA SELAMANYA</p>
+        <p>Balance</p>
+        <p className="text-2xl font-semibold">20000</p>
+        <p>Subcription Status</p>
+        <p className="text-2xl font-semibold">STANDARD</p>
+      </div>
+    </>
+  );
 }
