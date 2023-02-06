5344502c6a48f30232798b97918bc66fa43e903b
ghazytp <ghazytp@gmail.com>
2023-02-06 15:01:05 +0700

    remove edit from contact page

--- src/views/ContactPage.jsx
+++ src/views/ContactPage.jsx
@@ -5,8 +5,6 @@ import axios from "axios";
 export default function ContactPage() {
   const [contactList, setContactList] = useState([]);
   const [contactDetails, setContactDetails] = useState({});
-  const [editValue, setEditValue] = useState(false);
-
 
   useEffect(() => {
     (async () => {
@@ -14,9 +12,8 @@ export default function ContactPage() {
         "https://jsonplaceholder.typicode.com/users"
       );
       setContactList(data);
-      setContactDetails(data[0])
+      setContactDetails(data[0]);
     })();
-
   }, []);
   return (
     <>
@@ -38,7 +35,11 @@ export default function ContactPage() {
                     setContactDetails(el);
                   }}
                   key={i}
-                  className={contactDetails.id == el.id ? "hover:bg-slate-100 px-4 py-3 rounded-lg bg-slate-100" : "hover:bg-slate-100 px-4 py-3 rounded-lg"}
+                  className={
+                    contactDetails.id == el.id
+                      ? "hover:bg-slate-100 px-4 py-3 rounded-lg bg-slate-100"
+                      : "hover:bg-slate-100 px-4 py-3 rounded-lg"
+                  }
                 >
                   <UserPreview name={el.name} company={el.company.name} />
                 </div>
@@ -48,10 +49,11 @@ export default function ContactPage() {
         </div>
 
         <div className="border-2 rounded-xl w-full relative">
-            <div className="absolute top-4 right-4 bg-white shadow-md rounded-lg">
-                <button onClick={() => setEditValue(!editValue)} className="pl-3 py-1 pr-2 border-r-2 border-theme-1  rounded-l-lg hover:bg-green-400 hover:text-white w-16">Edit</button>
-                <button className="pr-3 py-1 pl-2  rounded-r-lg hover:bg-red-400  hover:text-white">Delete</button>
-            </div>
+          <div className="absolute top-4 right-4 bg-white shadow-md rounded-lg">
+            <button className="px-3 py-1 rounded-lg hover:bg-red-300 bg-red-400 text-white hover:text-white">
+              Delete
+            </button>
+          </div>
           <div className="rounded-t-xl flex flex-col items-center justify-center bg-sky-50 px-8 pt-8 pb-4">
             <div className="bg-red-300 mx-auto w-36 h-36 rounded-full overflow-hidden ring-theme-1 ring-offset-4 ring-2 border-theme-1 ">
               <img
@@ -67,59 +69,27 @@ export default function ContactPage() {
           <div className="px-8 py-4 tracking-wide flex flex-col gap-3 mt-2">
             <div>
               <p className="">Full Name</p>
-              {!editValue ? (
-                <p className=" text-2xl font-semibold hover:underline ">
-                  {contactDetails.name}
-                </p>
-              ) : (
-                <input
-                  type="text"
-                  className="outline-none border-2 text-xl px-4 py-1 font-semibold rounded-xl w-full"
-                  value={contactDetails.name}
-                />
-              )}
+              <p className=" text-2xl font-semibold">
+                {contactDetails.name}
+              </p>
             </div>
             <div>
               <p className="">Organization</p>
-              {!editValue ? (
-                <p className=" text-2xl font-semibold hover:underline ">
-                  {contactDetails?.company?.name}
-                </p>
-              ) : (
-                <input
-                  type="text"
-                  className="outline-none border-2 text-xl px-4 py-1 font-semibold rounded-xl w-full"
-                  value={contactDetails?.company?.name}
-                />
-              )}
+              <p className=" text-2xl font-semibold">
+                {contactDetails?.company?.name}
+              </p>
             </div>
             <div>
               <p className="">Email Address</p>
-              {!editValue ? (
-                <p className=" text-2xl font-semibold hover:underline ">
-                  {contactDetails.email}
-                </p>
-              ) : (
-                <input
-                  type="text"
-                  className="outline-none border-2 text-xl px-4 py-1 font-semibold rounded-xl w-full"
-                  value={"johnclaymore@mail.com"}
-                />
-              )}
+              <p className=" text-2xl font-semibold">
+                {contactDetails.email}
+              </p>
             </div>
             <div>
               <p className="">Phone Number</p>
-              {!editValue ? (
-                <p className=" text-2xl font-semibold hover:underline ">
-                  +62822323232
-                </p>
-              ) : (
-                <input
-                  type="text"
-                  className="outline-none border-2 text-xl px-4 py-1 font-semibold rounded-xl w-full"
-                  value={"+62822323232"}
-                />
-              )}
+              <p className=" text-2xl font-semibold">
+                +62822323232
+              </p>
             </div>
           </div>
         </div>
