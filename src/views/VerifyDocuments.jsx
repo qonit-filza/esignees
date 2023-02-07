47d1f5e45b7b92fe39525124ba92e85b0a79e28e
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-07 18:33:51 +0700

    handle download from server

--- src/views/VerifyDocuments.jsx
+++ src/views/VerifyDocuments.jsx
@@ -1,23 +1,48 @@
-import { useState } from "react";
-import images from "../assets/img/authentication_img.png";
-import logo from "../assets/img/logo-3.png"
+import { useState } from 'react';
+import images from '../assets/img/authentication_img.png';
+import logo from '../assets/img/logo-3.png';
+import axios from 'axios';
+const access_token = localStorage.getItem('access_token');
 
 export default function VerifyDocumentPage() {
-  const [fileName, setFileName] = useState("");
+  const [file, setFile] = useState({});
 
   const checkFileName = (e) => {
-    setFileName(e.target.files[0].name);
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
   };
+
+  const handleVerification = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const { data } = await axios.post(
+        'http://localhost:3000/documents',
+        formData,
+        {
+          headers: { 'Content-Type': 'multipart/form-data', access_token },
+        }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <nav className="pt-8 pb-6 px-12 flex shadow-md items-center justify-between">
-        <img src={logo} alt="esignee_logo" className="w-32 cursor-pointer"/>
+        <img src={logo} alt="esignee_logo" className="w-32 cursor-pointer" />
         <div className="">
-            <ul className="flex gap-6">
-                <li className="cursor-pointer hover:font-semibold">Home</li>
-                <li className="cursor-pointer hover:font-semibold">Verify Documents</li>
-                <li className="cursor-pointer hover:font-semibold">Sign In</li>
-            </ul>
+          <ul className="flex gap-6">
+            <li className="cursor-pointer hover:font-semibold">Home</li>
+            <li className="cursor-pointer hover:font-semibold">
+              Verify Documents
+            </li>
+            <li className="cursor-pointer hover:font-semibold">Sign In</li>
+          </ul>
         </div>
       </nav>
       <div className="flex flex-col items-center justify-center h-[80vh] w-[35vw] mx-auto gap-6">
@@ -25,7 +50,7 @@ export default function VerifyDocumentPage() {
           <p>Document Signature Verification</p>
         </div>
         <div className="w-full flex items-center justify-center">
-          <img src={images} alt="" className="w-[60%]"/>
+          <img src={images} alt="" className="w-[60%]" />
         </div>
         <div className="w-full">
           <input
@@ -40,7 +65,7 @@ export default function VerifyDocumentPage() {
             className="flex rounded-full bg-white overflow-hidden border-2 items-center h-fit justify-between shadow-sm hover:bg-sky-50"
           >
             <div className="break-normal overflow-hidden px-4 text-lg">
-              {!fileName ? <p>Choose Your File</p> : <p>{fileName}</p>}
+              {!file.name ? <p>Choose Your File</p> : <p>{file.name}</p>}
             </div>
             <div className="bg-theme-3 px-3 py-2 text-white text-md">
               Upload File
@@ -48,7 +73,10 @@ export default function VerifyDocumentPage() {
           </label>
         </div>
 
-        <button className="bg-green-400 py-2 w-full rounded-full text-xl text-white tracking-wide hover:bg-green-300 shadow-sm">
+        <button
+          onClick={handleVerification}
+          className="bg-green-400 py-2 w-full rounded-full text-xl text-white tracking-wide hover:bg-green-300 shadow-sm"
+        >
           Verify
         </button>
       </div>
