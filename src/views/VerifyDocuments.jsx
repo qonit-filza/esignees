ee4109af3738b34f5a44085214a7c3c1b32d8d58
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-07 22:48:34 +0700

    show doc verify on browser

--- src/views/VerifyDocuments.jsx
+++ src/views/VerifyDocuments.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react';
 import images from '../assets/img/authentication_img.png';
 import NavbarPublic from '../components/NavbarPublic';
 import axios from 'axios';
+import { localeDateTime } from '../helpers/dateHelper';
 const access_token = localStorage.getItem('access_token');
 
 export default function VerifyDocumentPage() {
   const [file, setFile] = useState({});
+  const [results, setResults] = useState('');
+  const [invalidDocError, setInvalidDocError] = useState('');
 
   const checkFileName = (e) => {
     if (e.target.files) {
       setFile(e.target.files[0]);
+      setResults('');
+      setInvalidDocError('');
     }
   };
 
@@ -25,9 +30,11 @@ export default function VerifyDocumentPage() {
           headers: { 'Content-Type': 'multipart/form-data', access_token },
         }
       );
-      console.log(data);
+      setResults(data.detail);
     } catch (error) {
-      console.log(error);
+      if (error.response.status === 400) {
+        setInvalidDocError(error.response.data.message);
+      }
     }
   };
 
@@ -41,6 +48,7 @@ export default function VerifyDocumentPage() {
         <div className="w-full flex items-center justify-center">
           <img src={images} alt="" className="w-[60%]" />
         </div>
+
         <div className="w-full">
           <input
             onChange={checkFileName}
@@ -62,12 +70,43 @@ export default function VerifyDocumentPage() {
           </label>
         </div>
 
-        <button
-          onClick={handleVerification}
-          className="bg-green-500 py-2 w-full rounded-full text-xl text-white tracking-wide hover:bg-green-400 shadow-sm"
-        >
-          Verify
-        </button>
+        {results ? (
+          <button
+            disabled
+            className="bg-green-500 py-2 w-full rounded-full text-xl text-white tracking-wide "
+          >
+            Document Verified - {results.length}
+            {results.length > 1 ? ' Signatures' : ' Signature'} Found
+          </button>
+        ) : invalidDocError ? (
+          <button
+            disabled
+            className="bg-red-400 py-2 w-full rounded-full text-xl text-white tracking-wide"
+          >
+            {invalidDocError}
+          </button>
+        ) : (
+          <button
+            onClick={handleVerification}
+            className="bg-green-500 py-2 w-full rounded-full text-xl text-white tracking-wide hover:bg-green-400 shadow-sm"
+          >
+            Verify
+          </button>
+        )}
+
+        {results && (
+          <div className="w-full">
+            <div className=" flex flex-col justify-between px-4 w-full gap-2">
+              {results.map((el) => (
+                <div className={'flex flex-row w-full justify-between'}>
+                  <div>Signed by {el.signedBy}</div>
+                  <div> {el.signedByEmail}</div>
+                  <div>{localeDateTime(el.signedDate)}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
