ad91fda19cc30965e2d0b290f787e4a6de0bf86a
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-02-08 20:16:08 +0700

    handle show hide company page

--- src/views/OrganizationPage.jsx
+++ src/views/OrganizationPage.jsx
@@ -1,43 +1,49 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
+import { useEffect, useState } from 'react';
+import SubcriptionPage from '../components/SubcriptionPage';
+import axios from 'axios';
 
 export default function OrganizationPage() {
-
   const [company, setCompany] = useState({});
 
-  const FetchCompany =  async () => {
+  const FetchCompany = async () => {
     try {
-      let {data} = await axios({
-        url : `http://localhost:3000/companies`, 
-        method : "get",
-        headers : {
-          'access_token' : localStorage.getItem("access_token")
+      let { data } = await axios({
+        url: `http://localhost:3000/companies`,
+        method: 'get',
+        headers: {
+          access_token: localStorage.getItem('access_token'),
         },
-      })
-      setCompany(data)
+      });
+      setCompany(data);
+      console.log(data);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    FetchCompany()
+    FetchCompany();
   }, []);
   return (
     <>
-      <div className="flex flex-col gap-2">
-        <p>Organization Name</p>
-        <p className="text-2xl font-semibold">{company.nameCompany}</p>
-        <p>Company Invite Code</p>
-        <p className="text-2xl font-semibold">{company.companyInviteCode}</p>
-        <p>Industry</p>
-        <p className="text-2xl font-semibold">{company.industry}</p>
-        <p>Subcription Status</p>
-        <p className="text-2xl font-semibold">{company.status}</p>
-        <p>Expired date</p>
-        <p className="text-2xl font-semibold">{company.dueDate?.slice(0, 10)}</p>
-      </div>
+      {company.status === 'Subscription' ? (
+        <div className="flex flex-col gap-2">
+          <p>Organization Name</p>
+          <p className="text-2xl font-semibold">{company.nameCompany}</p>
+          <p>Company Invite Code</p>
+          <p className="text-2xl font-semibold">{company.companyInviteCode}</p>
+          <p>Industry</p>
+          <p className="text-2xl font-semibold">{company.industry}</p>
+          <p>Subcription Status</p>
+          <p className="text-2xl font-semibold">{company.status}</p>
+          <p>Expired date</p>
+          <p className="text-2xl font-semibold">
+            {company.dueDate?.slice(0, 10)}
+          </p>
+        </div>
+      ) : (
+        <SubcriptionPage />
+      )}
     </>
   );
 }
